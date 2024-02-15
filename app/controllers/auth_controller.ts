5165fb52a9f5c305f079cdcbb85d19241e8cfed1
Sigve Hansen <sighan12@gmail.com>
2024-02-15 08:56:15 +0100

    auth

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -7,32 +7,41 @@ export default class AuthController {
     return view.render('pages/auth/signup')
   }
 
-  async signup({ request, response, auth }: HttpContext) {
+  async signup({ request, response, auth, session }: HttpContext) {
     const userSchema = vine.object({
-      name: vine.string().trim(),
+      fullName: vine.string().trim(),
       email: vine.string().trim().email(),
       password: vine.string().trim().minLength(8),
     })
 
     const data = await vine.validate({
       schema: userSchema,
-      data: request.only(['name', 'email', 'password']),
+      data: request.only(['fullName', 'email', 'password']),
     })
 
-    const user = await User.create(data)
+    try {
+      const user = await User.create(data)
+      await auth.use('web').login(user)
 
-    await auth.use('web').login(user)
-
-    response.redirect('/dashboard')
+      response.redirect().toRoute('dashboard')
+    } catch {
+      session.flash('error', 'Email is already in use')
+      response.redirect().back()
+    }
   }
 
-  async login({ request, response, auth }: HttpContext) {
+  async login({ request, response, auth, session }: HttpContext) {
     const { email, password } = request.only(['email', 'password'])
 
-    const user = await User.verifyCredentials(email, password)
-    await auth.use('web').login(user)
+    try {
+      const user = await User.verifyCredentials(email, password)
+      await auth.use('web').login(user)
 
-    response.redirect('/dashboard')
+      response.redirect('/dashboard')
+    } catch {
+      session.flash('error', 'Invalid credentials')
+      response.redirect().back()
+    }
   }
 
   async loginShow({ view }: HttpContext) {
--- start/routes.ts
+++ start/routes.ts
@@ -8,16 +8,37 @@
 */
 
 const AuthController = () => import('#controllers/auth_controller')
+import User from '#models/user'
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
 
+const users = User.all()
+
+// Marketing
 router.on('/').render('pages/home').as('home')
-router.on('/dashboard').render('pages/dashboard').as('dashboard')
 
 // Auth
-router.get('/signup', [AuthController, 'signupShow']).as('auth.signup')
-router.get('/login', [AuthController, 'loginShow']).as('auth.login')
-router.get('/forgot-password', [AuthController, 'forgotPasswordShow']).as('auth.forgot-password')
-router.get('/reset-password', [AuthController, 'resetPasswordShow']).as('auth.reset-password')
+router
+  .group(() => {
+    router.post('/signup', [AuthController, 'signup']).as('auth.signup')
+    router.get('/signup', [AuthController, 'signupShow']).as('auth.signup.show')
+    router.post('/login', [AuthController, 'login']).as('auth.login')
+    router.get('/login', [AuthController, 'loginShow']).as('auth.login.show')
+    router
+      .get('/forgot-password', [AuthController, 'forgotPasswordShow'])
+      .as('auth.forgot-password.show')
+    router
+      .get('/reset-password', [AuthController, 'resetPasswordShow'])
+      .as('auth.reset-password.show')
+  })
+  .middleware([middleware.guest()])
 
-router.on('/user/settings').render('pages/user/settings').as('user.settings')
-router.on('/user/profile').render('pages/user/profile').as('user.profile')
+// App
+router
+  .group(() => {
+    router.on('/dashboard').render('pages/dashboard', { users }).as('dashboard')
+
+    router.on('/user/settings').render('pages/user/settings').as('user.settings')
+    router.on('/user/profile').render('pages/user/profile').as('user.profile')
+  })
+  .middleware([middleware.auth()])
